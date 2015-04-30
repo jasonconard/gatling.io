1110f4e49b729e1da194f2095a90465d6cadf3eb
Flavien Bert <bert.flavien@gmail.com>
2015-04-30 14:47:45 +0200

    Edit the pagination to add the title in the button

--- app/scripts/controllers/StoryCtrl.js
+++ app/scripts/controllers/StoryCtrl.js
@@ -1,8 +1,6 @@
 var StoryCtrl = function ($scope, $stateParams, $state, $location, $http) {
 
   var story = $stateParams.story;
-  $scope.previousClass = "";
-  $scope.nextClass = "";
 
   $http.get('../../views/stories/stories.json').success(function (data) {
     pagination(data.stories);
@@ -10,14 +8,12 @@ var StoryCtrl = function ($scope, $stateParams, $state, $location, $http) {
 
   var pagination = function (stories) {
     for (var index = 0; index < stories.length; index++) {
-      if (story == stories[index] && stories.length > 1) {
+      if (story == stories[index].url && stories.length > 1) {
         if (index == 0) {                        // The story is the first element
           $scope.next = stories[index + 1];
-          $scope.previousClass = "disabled";
         }
         else if (index == stories.length - 1) {   // The story is the last element
           $scope.previous = stories[index - 1];
-          $scope.nextClass = "disabled";
         }
         else {
           $scope.next = stories[index + 1];
